95c1ffe2bd0c32a94868b4b56403556eef49ba0d
oolong <oolong@yahoo.co.jp>
2013-04-19 11:41:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onecup::Application.config.secret_token = '2cdef5b1faf4313dd9ad607c1efcca36458ded7a76915a002c79e2f3d48cfb4cd0d97d71781d6c9b0b273b267e1c7a52b791fb74c4ad72359585a90e009b2d28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Onecup::Application.config.session_store :cookie_store, key: '_onecup_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Onecup::Application.config.session_store :active_record_store
